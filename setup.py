864854484dedac55a6b480c96e7be90a8de3c739
HJoonKwon <dsazz801@gmail.com>
2023-04-21 05:16:06 +0000

    update: setup py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
 setup(
-    name="aspanformer",
-    packages=["aspanformer"],
+    name="ml-aspanformer",
+    packages=["ml-aspanformer"],
     version="0.1.0",
+    package_dir={'':'src'},
     author="J",
     install_requires=open("requirements.txt", "r").read().split("\n"),
 )
